629b446e912ee8bdd21f0451f7fb89437254bc19
neotamoe <neota.moe@gmail.com>
2018-05-30 15:42:41 -0500

    adding players beyond color

--- scripts.js
+++ scripts.js
@@ -1,21 +1,54 @@
 function popIt() {
-    console.log("dice rolled");
+    if(!this.currentPlayer){
+        return;
+    }
+    console.log("dice rolled for " + this.currentPlayer);
     var die = 1 + Math.floor(Math.random() * 6);
-    popperText = die;
-    $("#btnPopper").text(popperText);
-    setTimeout(resetPopper,1000);
+    popperDisplay = die;
+    console.log(popperDisplay);
+    $("#btnPopper").text(popperDisplay);
+    if(popperDisplay === 6){
+        // player able to move out of home or move 6 spaces
+        document.getElementById("instructions").innerHTML = "Click home to move a piece into play OR click the piece you want to move.";
+        setTimeout(clearInstructions,1000);
+    } else {
+        // if(this.currentPlayer.hasTokensOut){
+            // select token to move
+            document.getElementById("instructions").innerHTML = "Click the piece you want to move.";
+            setTimeout(clearInstructions,1000);
+
+        // } else {
+        //     // end of turn, trigger next player
+        //     document.getElementById("instructions").innerHTML = "";
+            resetPopper();
+        // }
+    }
+    var index = this.fullPlayers.findIndex(player => player.color === this.currentPlayer);
+    console.log(index);
+    if(index == this.players.length-1){
+        $('#currentPlayer').removeClass(this.fullPlayers[index].color);
+        this.currentPlayer = this.fullPlayers[0].color;
+        $('#currentPlayer').addClass(this.fullPlayers[0].color);
+    } else {
+        $('#currentPlayer').removeClass(this.fullPlayers[index].color);
+        this.currentPlayer = this.fullPlayers[index+1].color;
+        $('#currentPlayer').addClass(this.fullPlayers[index+1].color);
+    }
+    
+    // setTimeout(resetPopper,1000);
 }
 
-var popperText="POP";
+var popperDisplay="POP";
 var yellowCount="4";
 var blueCount="4";
 var greenCount="4";
 var redCount="4";
 var players = [];
 var currentPlayer = "";
+var fullPlayers = [];
 
 $(document).ready(function(){
-   $("#btnPopper").text(popperText);
+   $("#btnPopper").text(popperDisplay);
    $("#yellowCount").text(yellowCount);
    $("#greenCount").text(greenCount);
    $("#blueCount").text(blueCount);
@@ -24,6 +57,10 @@ $(document).ready(function(){
 
 function resetPopper() {
     $("#btnPopper").text("POP");
+}
+
+function clearInstructions() {
+    document.getElementById("instructions").innerHTML = "";
 }
 
 function toggle(value) {
@@ -43,21 +80,38 @@ function play(players) {
     if(players.length<1){
         return;
     } 
-    console.log(players);
     $('input.players').attr('disabled', true);    
     $('#btnPlay').attr('disabled', true);
     for(var i=0; i < players.length; i++){
         var element = document.getElementById(players[i]);
-        console.log(element);
         element.classList.remove('notPlaying');
+        var color = players[i];
+        switch(color) {
+            case "blue":
+                this.fullPlayers[i] = this.bluePlayer;
+                break;
+            case "red":
+                this.fullPlayers[i] = this.redPlayer;
+                break;
+            case "yellow":
+                this.fullPlayers[i] = this.yellowPlayer;
+                break;
+            case "green":
+                this.fullPlayers[i] = this.greenPlayer;
+                break;
+        }
     }
     $('#currentPlayer').addClass(players[0]);
+    document.getElementById("instructions").innerHTML = "";
+    this.currentPlayer = this.fullPlayers[0].color;
 }
 
 function Player(color, startSquare, homeRow) {
     this.color = color;
     this.startSquare = startSquare;
     this.homeRow = homeRow;
+    this.hasTokensOut = false;
+    this.numberOfTokensOut = 0;
 }
 
 var bluePlayer = new Player('blue', 1, ['29', '30', '31', '32']);
